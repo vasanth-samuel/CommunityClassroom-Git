271d0c84f50f9292958ec0655cd4ad2ec15b8806
vasanth-samuel <chebroluvasanthms@gmail.com>
2025-11-23 12:54:11 +0530

    Add files via upload

---
+++ loopjump.java
@@ -0,0 +1,54 @@
+class loopjump{
+
+public static void main(String[] args) {
+    
+   /*  // for loop
+     int n = 7;  
+     for (int i = 3; i <= n; i++) 
+     {
+      System.out.println("after loop" + i);
+     }
+
+ // while loop
+      int i=5;
+      while (i < 10) 
+     {
+      System.out.println(i);
+      i++; // if not this condition infinite loops
+     }
+
+     // do while loop
+     int i = 1;
+    do 
+     {
+     System.out.println(i);
+    i++;
+     }
+     while (i < 8); 
+      
+
+     // jumping statement //
+// break
+      for (int i = 0; i < 10; i++) 
+     { 
+     if (i == 8)
+      {
+       break;
+      }
+        System.out.println(i);
+     }  */
+// continue
+ for (int i = 2; i < 10; i++) {
+     if (i == 5) 
+     {
+     continue;
+     }
+     System.out.println(i);
+     }
+
+
+ 
+}
+     
+    
+}
